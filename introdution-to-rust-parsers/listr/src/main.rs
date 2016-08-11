1572974f07fbe06a88f91a2e7afa68b848238621
Bruno Tavares <connect+github@bltavares.com>
2016-08-10 22:12:17 -0300

    Make a request with a client

--- introdution-to-rust-parsers/listr/src/main.rs
+++ introdution-to-rust-parsers/listr/src/main.rs
@@ -1,5 +1,12 @@
 extern crate hyper;
 
+use hyper::client::Client;
+
 fn main() {
-    println!("Hello, world!");
+    let client = Client::new();
+    let request = client.get("http://www.amazon.com.br/registry/wishlist/3DA4I0ZLH8ADW/ref=cm_sw_r_tw_ws_9hJzwb06V29HS");
+    let request_result = request.send();
+    let response = request_result.expect("The HTTP request failed to be made");
+
+    println!("Response status: {}", response.status);
 }
