a1ff5a24a27d92a53349c102f7b558e968903082
Bruno Tavares <connect+github@bltavares.com>
2016-08-10 22:35:09 -0300

    Checkpoint 1 - File download

--- introdution-to-rust-parsers/listr/src/main.rs
+++ introdution-to-rust-parsers/listr/src/main.rs
@@ -1,16 +1,15 @@
 extern crate hyper;
 
 use hyper::client::Client;
+use std::io::Read;
 
 fn main() {
     let client = Client::new();
     let request = client.get("http://www.amazon.com.br/registry/wishlist/3DA4I0ZLH8ADW/ref=cm_sw_r_tw_ws_9hJzwb06V29HS");
     let request_result = request.send();
-    let response = request_result.expect("The HTTP request failed to be made");
+    let mut response = request_result.expect("The HTTP request failed to be made");
 
     println!("Response status: {}", response.status);
-
-    use std::io::Read;
 
     let mut body = String::new();
     response.read_to_string(&mut body).expect("Could not put the body content into the string");
